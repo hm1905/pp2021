99f5bba4303325732288f920ef617e473bcc9934
hm1905 <nghoangminh1905@gmail.com>
2021-03-24 22:46:33 +0700

    Submit practical work 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -26,53 +26,44 @@ def course_info(l_course, number_c):
     return l_course
 
 def student_mark(l_student, l_course, mark_list):
-    if l_course == [] or l_student == []:
-        print("Not enough information about students or courses was given please try again")
-    else:
-        print("Available Courses: ")
-        print(l_course)
+    print("Available Courses: ")
+    print(l_course)
+    c_name = input("Please select one to enter mark<by name>: ")
+    while not any(i['name'] == c_name for i in l_course):
+        print("Please try again: ")
         c_name = input("Please select one to enter mark<by name>: ")
-        while not any(i['name'] == c_name for i in l_course):
+    i = str(1)
+    count = 0
+    while i == str(1):
+        print("Student list: ")
+        print(l_student)
+        s_name = input("Please select which student to mark<by name>: ")
+        while not any(i['name'] == s_name for i in l_student):
             print("Please try again: ")
-            c_name = input("Please select one to enter mark<by name>: ")
-        i = str(1)
-        count = 0
-        while i == str(1):
-            print("Student list: ")
-            print(l_student)
             s_name = input("Please select which student to mark<by name>: ")
-            while not any(i['name'] == s_name for i in l_student):
-                print("Please try again: ")
-                s_name = input("Please select which student to mark<by name>: ")
-            mk = input("The mark: ")
-            mark_list.append({'Subject': c_name,'Name': s_name,'Mark': mk})
-            count += 1
-            if count >= number_s:
-                print("You have entered mark for every students for " + c_name)
+        mk = input("The mark: ")
+        mark_list.append({'Subject': c_name,'Name': s_name,'Mark': mk})
+        count += 1
+        if count >= number_s:
+            print("You have entered mark for every students for " + c_name)
+        i = input("Do you wish to continue marking the student<1:Yes, 0:No>: ")
+        while not (i == str(1) or i == str(0)):
+            print("Please enter only 1 or 0")
             i = input("Do you wish to continue marking the student<1:Yes, 0:No>: ")
-            while not (i == str(1) or i == str(0)):
-                print("Please enter only 1 or 0")
-                i = input("Do you wish to continue marking the student<1:Yes, 0:No>: ")
-        return mark_list
+    return mark_list
 
 
 def list_courses(l_course):
-    if l_course == []:
-        print("No information about course was given")
-    else:
-        print("Available course: ")
-        print(" ")
-        for i in  l_course:
-            print(i)
+    print("Available course: ")
+    print(" ")
+    for i in  l_course:
+        print(i)
 
 def list_students(l_student):
-    if l_student == []:
-        print("No information about students was given")
-    else:
-        print("Students list: ")
-        print(" ")
-        for i in  l_student:
-            print(i)
+    print("Students list: ")
+    print(" ")
+    for i in  l_student:
+        print(i)
 
 def sh_student_mark(x):
     show=[]
@@ -132,29 +123,51 @@ if __name__ == '__main__':
 
         if i==1:
             number_s=no_student()
+            if i not in op:
+                op.append(i)
         if i==2:
             try:
                 student_info(l_student, number_s)
+                if i not in op:
+                    op.append(i)
             except:
                 print("No number of student was given. Please insert it before try again.")
         if i==3:
             number_c=no_course()
+            if i not in op:
+                op.append(i)
         if i==4:
             try:
                 course_info(l_course, number_c)
+                if i not in op:
+                    op.append(i)
             except:
                 print("No number of courses was given. Please insert it before try again.")
         if i==5:
-            x=student_mark(l_student, l_course, mark_list)
+            if l_course == [] or l_student == []:
+                print("Not enough information about students or courses was given please try again")
+            else:
+                x=student_mark(l_student, l_course, mark_list)
+                if i not in op:
+                    op.append(i)
         if i==6:
-            list_courses(l_course)
+            if l_course == []:
+                print("No information about course was given")
+            else:
+                list_courses(l_course)
+                if i not in op:
+                    op.append(i)
         if i==7:
-            list_students(l_student)
+            if l_student == []:
+                print("No information about students was given")
+            else:
+                list_students(l_student)
+                if i not in op:
+                    op.append(i)
         if i==8:
             try:
                 sh_student_mark(x)
+                if i not in op:
+                    op.append(i)
             except:
                 print("You havent mark anyone")
-
-        if i not in op:
-            op.append(i)
