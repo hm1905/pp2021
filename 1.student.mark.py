1a77fa739e76c911b4bc2c2432e06def085991fe
hm1905 <nghoangminh1905@gmail.com>
2021-04-02 00:53:27 +0700

    Changes to __name__

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,29 +1,35 @@
-#Function
+# Function
 
 def no_student():
     student_no = int(input("Please enter number of student: "))
     return student_no
 
+
 def no_course():
     course_no = int(input("Please enter number of courses: "))
     return course_no
 
-  #Student info include : id, name, DoB
+  # Student info include : id, name, DoB
+
+
 def student_info(l_student, number_s):
-    for i in range(1, number_s+1):
+    for i in range(1, number_s + 1):
         info_s = {'id': input("Please enter student id: "),
                   'name': input("Please enter student name: "),
-                  'DoB' : input("please enter student's date of birth: ")}
+                  'DoB': input("please enter student's date of birth: ")}
         l_student.append(info_s)
     return l_student
 
-  #Course info include : id, name
+  # Course info include : id, name
+
+
 def course_info(l_course, number_c):
-    for i in range(1, number_c+1):
+    for i in range(1, number_c + 1):
         info_c = {'id': input("Please enter courese id: "),
                   'name': input("Please enter course name: ")}
         l_course.append(info_c)
     return l_course
+
 
 def student_mark(l_student, l_course, mark_list):
     print("Available Courses: ")
@@ -44,31 +50,34 @@ def student_mark(l_student, l_course, mark_list):
             print("Please try again: ")
             s_name = input("Please select which student to mark<by name>: ")
         mk = input("The mark: ")
-        mark_list.append({'Subject': c_name,'Name': s_name,'Mark': mk})
+        mark_list.append({'Subject': c_name, 'Name': s_name, 'Mark': mk})
         count += 1
         if count >= number_s:
             print("You have entered mark for every students for " + c_name)
         i = input("Do you wish to continue marking the student<1:Yes, 0:No>: ")
         while not (i == str(1) or i == str(0)):
             print("Please enter only 1 or 0")
-            i = input("Do you wish to continue marking the student<1:Yes, 0:No>: ")
+            i = input(
+                "Do you wish to continue marking the student<1:Yes, 0:No>: ")
     return mark_list
 
 
 def list_courses(l_course):
     print("Available course: ")
     print(" ")
-    for i in  l_course:
+    for i in l_course:
         print(i)
+
 
 def list_students(l_student):
     print("Students list: ")
     print(" ")
-    for i in  l_student:
+    for i in l_student:
         print(i)
 
+
 def sh_student_mark(mark_list):
-    show=[]
+    show = []
     print("Marked subject")
     mk_s = []
     for i in mark_list:
@@ -78,7 +87,7 @@ def sh_student_mark(mark_list):
     name = input("Please select which subject you want to see: ")
     print(" ")
     while not any(i['Subject'] == name for i in mark_list):
-        print("Please try again. You might havent marked "+ name +" yet")
+        print("Please try again. You might havent marked " + name + " yet")
         name = input("Please select which subject you want to see: ")
         print(" ")
     for i in mark_list:
@@ -87,18 +96,7 @@ def sh_student_mark(mark_list):
     for i in show:
         print(i)
 
-
-#Variable
- #List of Students
-l_student = []
- #List of Courses
-l_course = []
- #List of mark
-mark_list = []
-
-#UI
-
-if __name__ == '__main__':
+def main():
     i = None
     op = []
     while i != 9:
@@ -124,53 +122,71 @@ if __name__ == '__main__':
             print("Please try again")
             i = int(input("Choose your action: "))
 
-        if i==1:
-            number_s=no_student()
+        if i == 1:
+            number_s = no_student()
             if i not in op:
                 op.append(i)
-        if i==2:
+        if i == 2:
             try:
                 student_info(l_student, number_s)
                 if i not in op:
                     op.append(i)
             except:
-                print("No number of student was given. Please insert it before try again.")
-        if i==3:
-            number_c=no_course()
+                print(
+                    "No number of student was given. Please insert it before try again.")
+        if i == 3:
+            number_c = no_course()
             if i not in op:
                 op.append(i)
-        if i==4:
+        if i == 4:
             try:
                 course_info(l_course, number_c)
                 if i not in op:
                     op.append(i)
             except:
-                print("No number of courses was given. Please insert it before try again.")
-        if i==5:
+                print(
+                    "No number of courses was given. Please insert it before try again.")
+        if i == 5:
             if l_course == [] or l_student == []:
-                print("Not enough information about students or courses was given please try again")
+                print(
+                    "Not enough information about students or courses was given please try again")
             else:
                 student_mark(l_student, l_course, mark_list)
                 if i not in op:
                     op.append(i)
-        if i==6:
+        if i == 6:
             if l_course == []:
                 print("No information about course was given")
             else:
                 list_courses(l_course)
                 if i not in op:
                     op.append(i)
-        if i==7:
+        if i == 7:
             if l_student == []:
                 print("No information about students was given")
             else:
                 list_students(l_student)
                 if i not in op:
                     op.append(i)
-        if i==8:
+        if i == 8:
             if mark_list == []:
                 print("You havent marked anyone!")
             else:
                 sh_student_mark(mark_list)
                 if i not in op:
                     op.append(i)
+        if i == 9:
+            print("Goodbye")
+
+# Variable
+ # List of Students
+l_student = []
+# List of Courses
+l_course = []
+# List of mark
+mark_list = []
+
+# UI
+
+if __name__ == '__main__':
+    main()
