2397dad42dd76b21f28c95613fa68e3826980b7d
hm1905 <nghoangminh1905@gmail.com>
2021-04-14 22:07:58 +0700

    Finished basic function

--- 3.test1.py
+++ 3.test1.py
@@ -2,12 +2,14 @@ import math
 import numpy
 import curses
 import time
+from operator import itemgetter
 
 
 # VARIABLES.
-l_course = []
-l_student = []
-mark_list = []
+l_course = [{'id': '11','name':  'math', 'Credit': 3}, {'id': '12','name':  'python', 'Credit': 4}]
+l_student = [{'id': '1','name':  'joe', 'DoB': '12/12'}, {'id': '2','name':  'beth', 'DoB': '11/12'}]
+mark_list = [{'Subject': 'math','Name': 'joe', 'Mark': 19}, {'Subject': 'python','Name': 'joe', 'Mark': 20}, {'Subject': 'python','Name': 'beth', 'Mark': 12}]
+l_student_gpa_included = []
 
 # CLASS CREATION
 
@@ -50,7 +52,7 @@ class Program:
 
     def show_info(self, list):
         for i in list:
-            screen.addstr(str(i))
+            screen.addstr(str(i) + '\n')
             screen.refresh()
         time.sleep(2)
 
@@ -137,31 +139,32 @@ class Course(Program):
 
 class Mark:
 
-    def __init__(self, l_student, l_course, number_s, number_c, mark_list):
+    def __init__(self, l_student, l_course, number_s, number_c, mark_list, l_student_gpa_included):
         self.l_student = l_student
         self.l_course = l_course
         self.number_s = number_s
         self.number_c = number_c
         self.mark_list = mark_list
+        self.l_student_gpa_included = l_student_gpa_included
 
     def student_mark(self):
         if self.l_course == [] or self.l_student == []:
-            screen.addstr("Not enough information about students or courses was given please try again")
+            screen.addstr("Not enough information about students or courses was given please try again" )
             screeb.refresh()
         else:
-            screen.addstr("Available Courses: ")
-            screen.addstr(str(self.l_course))
+            screen.addstr("Available Courses: " + '\n')
+            screen.addstr(str(self.l_course) + '\n')
             screen.addstr("Please select one to enter mark<by name>: ")
             c_name = screen.getstr().decode('utf-8')
             while not any(i['name'] == c_name for i in self.l_course):
-                screen.addstr("Please try again: ")
+                screen.addstr("Please try again: " + '\n')
                 screen.addstr("Please select one to enter mark<by name>: ")
                 c_name = screen.getstr()
             i = str(1)
             count = 0
             while i == str(1):
-                screen.addstr("Student list: ")
-                screen.addstr(str(self.l_student))
+                screen.addstr("Student list: " + '\n')
+                screen.addstr(str(self.l_student) + '\n')
                 screen.addstr("Please select which student to mark<by name>: ")
                 s_name = screen.getstr().decode('utf-8')
                 while not any(i['name'] == s_name for i in self.l_student):
@@ -174,33 +177,31 @@ class Mark:
                     {'Subject': c_name, 'Name': s_name, 'Mark': mk})
                 count += 1
                 if count >= self.number_s:
-                    screen.addstr("You have entered mark for every students for " + str(c_name))
+                    screen.addstr("You have entered mark for every students for " + str(c_name) + '\n')
                 screen.addstr(
-                    "Do you wish to continue marking the student<1:Yes, 0:No>: ")
+                    "Do you wish to continue marking the student<1:Yes, 0:No>: " + '\n')
                 i = screen.getstr().decode('utf-8')
                 while not (i == str(1) or i == str(0)):
-                    screen.addstr("Please enter only 1 or 0")
+                    screen.addstr("Please enter only 1 or 0" + '\n')
                     screen.addstr(
-                        "Do you wish to continue marking the student<1:Yes, 0:No>: ")
+                        "Do you wish to continue marking the student<1:Yes, 0:No>: " + '\n')
                     i = screen.getstr().decode('utf-8')
             return self.mark_list
 
     def sh_student_mark(self):
         show = []
-        screen.addstr("Marked subject")
+        screen.addstr("Marked subject:" + '\n')
         mk_s = []
         for i in self.mark_list:
             if i['Subject'] not in mk_s:
                 mk_s.append(i['Subject'])
-        screen.addstr(str(mk_s))
+        screen.addstr(str(mk_s) + '\n')
         screen.addstr("Please select which subject you want to see: ")
         name = screen.getstr().decode('utf-8')
-        #print(" ")
         while not any(i['Subject'] == name for i in self.mark_list):
-            screen.addstr("Please try again. You might havent marked " + str(name) + " yet")
+            screen.addstr("Please try again. You might havent marked " + str(name) + " yet" + '\n')
             screen.addstr("Please select which subject you want to see: ")
             name = screen.getstr().decode('utf-8')
-            #print(" ")
         for i in self.mark_list:
             if i['Subject'] == name:
                 show.append({'Name': i['Name'], 'Mark': i['Mark']})
@@ -213,28 +214,54 @@ class Mark:
     def average_gpa(self):
         total_mark = numpy.array([])
         total_credit = numpy.array([])
-        print("Marked student")
+        screen.addstr("Marked student" + '\n')
         mk_n = []
         for i in self.mark_list:
             if i['Name'] not in mk_n:
                 mk_n.append(i['Name'])
-        print(mk_n)
-        name = input("Please select which student's GPA you want: ")
-        print(" ")
+        screen.addstr(str(mk_n) + '\n')
+        screen.addstr("Please select which student's GPA you want: ")
+        name = screen.getstr().decode('utf-8')
         while not any(i['Name'] == name for i in self.mark_list):
-            print("Please try again")
-            name = input("Please select which student's GPA you want: ")
-            print(" ")
+            screen.addstr("Please try again" + '\n')
+            screen.addstr("Please select which student's GPA you want: ")
+            name = screen.getstr().decode('utf-8')
+
         for i in self.mark_list:
             if i['Name'] == name:
-                total_mark = numpy.append(total_mark, [i['Mark']])
-                if (y['name'] == i['Subject'] for y in l_course):
-                   total_credit = numpy.append(total_credit, [y['Credit'] for y in l_course])
-        final_gpa = numpy.sum(total_mark) /(numpy.sum(total_credit)/2)
-        print(final_gpa)
-        print(total_credit)
-        print(total_mark)
-        print(" ")
+                total_mark = numpy.append(total_mark, i['Mark'])
+                for y in l_course:
+                    if (y['name'] == i['Subject']):
+                        total_credit = numpy.append(total_credit, y['Credit'])
+        gpa = numpy.dot(total_mark, total_credit) / numpy.sum(total_credit)
+        screen.addstr(str(gpa))
+        screen.refresh()
+        time.sleep(2)
+
+    def all_student_gpa(self):
+        for z in self.l_student:
+            total_mark = numpy.array([])
+            total_credit = numpy.array([])
+            for i in self.mark_list:
+                if i['Name'] == z['name']:
+                    total_mark = numpy.append(total_mark, i['Mark'])
+                    for y in l_course:
+                        if (y['name'] == i['Subject']):
+                            total_credit = numpy.append(total_credit, y['Credit'])
+            gpa = numpy.dot(total_mark, total_credit) / numpy.sum(total_credit)
+            info = {'id': z['id'],
+                    'name': z['name'],
+                    'DoB': z['DoB'],
+                    'GPA': gpa}
+            l_student_gpa_included.append(info)
+
+    def show_info(self):
+        l_student_gpa_included_sorted = []
+        l_student_gpa_included_sorted = sorted(l_student_gpa_included, key=itemgetter('GPA'), reverse = True)
+        for i in l_student_gpa_included_sorted:
+            screen.addstr(str(i) + '\n')
+            screen.refresh()
+        time.sleep(2)
 
 
 
@@ -261,6 +288,7 @@ menu = ['Define number of student in a class',
         'Show list of Students',
         'Show mark of students in a given course',
         'Show student GPA',
+        'Show list of Students with GPA',
         'Exit']
 
 def print_menu(stdscr, selected):
@@ -279,8 +307,11 @@ def print_menu(stdscr, selected):
     stdscr.refresh()
 
 def main(stdscr):
+    number_c = 0
+    number_s = 0
     students = Student()
     courses = Course()
+    marked = Mark(l_student, l_course, number_s, number_c, mark_list, l_student_gpa_included)
     global screen
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -319,21 +350,24 @@ def main(stdscr):
                 courses.set_information(l_course)
         elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 4:
             stdscr.refresh()
-            marked = Mark(l_student, l_course, number_s, number_c, mark_list)
             marked.student_mark()
         elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 5:
             stdscr.refresh()
-            curses.echo()
             students.show_info(l_course)
         elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 6:
             stdscr.refresh()
-            curses.echo()
             students.show_info(l_student)
         elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 7:
             stdscr.refresh()
-            curses.echo()
             marked.sh_student_mark()
+        elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 8:
+            stdscr.refresh()
+            marked.average_gpa()
         elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 9:
+            stdscr.refresh()
+            marked.all_student_gpa()
+            marked.show_info()
+        elif (key == curses.KEY_ENTER or key in [10,13]) and current_row == 10:
             break
         print_menu(stdscr, current_row)
         stdscr.refresh()
